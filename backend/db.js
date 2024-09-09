f8e74446bf2908925b592f089a9f347166bed04a
Vikas Chauhan <vcikas2626@gmail.com>
2024-09-09 18:57:44 +0530

    added register department route

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,21 @@
 const { sql } = require("@vercel/postgres");
+const bcrypt = require("bcrypt");
 
-async function handler(req, res) {
+const saltRounds = 10;
+
+async function registerDepartment(req, res) {
   try {
-    const result = await sql`SELECT * FROM invoices`;
-    res.json(result);
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
+    const newDepartment =
+      await sql`INSERT INTO departments (name, email, password) VALUES (${name}, ${email}, ${hashedPassword}) RETURNING *`;
+
+    res.json(newDepartment.rows[0]);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 }
 
-module.exports = handler;
+module.exports = registerDepartment;
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const handler = require("./db");
+const registerDepartment = require("./db");
+const bodyParse = require("body-parser");
+
 require("dotenv").config();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: true }));
 
 // Routes
 app.get("/", async (req, res) => {
   res.json({ name: "vikas" });
 });
 
-app.get("/invoices", handler);
+app.post("/register", registerDepartment);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
